a0df4b434f1455275fddfa7c3a2e3f4993a326f3
Rakhat Alpysbai <rakhatdev@gmail.com>
2023-02-20 12:34:44 +0600

    add starter boilerplate code

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { MantineProvider } from '@mantine/core';
+import { NotificationsProvider } from '@mantine/notifications';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <NotificationsProvider position="top-right">
+          <App />
+        </NotificationsProvider>
+      </MantineProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
