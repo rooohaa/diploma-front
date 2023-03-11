b3922a894f605bc2aeb8b9efe7dba3b7d7ac417c
Tamerlan Bulatkaliyev <tameoooo13@gmail.com>
2023-03-11 14:07:12 +0600

    fix: remove fullname sign page and layout

--- src/components/layout/AppHeader/index.tsx
+++ src/components/layout/AppHeader/index.tsx
@@ -3,40 +3,46 @@ import { Link } from 'react-router-dom';
 import { AppLogoLink } from 'components';
 import { HeaderWrapper } from './style';
 
-const AppHeader: React.FC = () => {
+interface IAppHeaderProps {
+  isAuth?: boolean;
+}
+
+const AppHeader: React.FC<IAppHeaderProps> = ({ isAuth = false }) => {
   return (
     <HeaderWrapper>
       <Container size="lg" style={{ width: '100%' }}>
         <div className="app-header-inner">
           <AppLogoLink />
 
-          <nav className="app-nav">
-            <ul>
-              <li>
-                <Link className="app-nav-link" to="/how-it-works">
-                  How it works
-                </Link>
-              </li>
+          {!isAuth ? (
+            <nav className="app-nav">
+              <ul>
+                <li>
+                  <Link className="app-nav-link" to="/how-it-works">
+                    How it works
+                  </Link>
+                </li>
 
-              <li>
-                <Link className="app-nav-link" to="/about">
-                  About us
-                </Link>
-              </li>
+                <li>
+                  <Link className="app-nav-link" to="/about">
+                    About us
+                  </Link>
+                </li>
 
-              <li>
-                <Link className="app-nav-link" to="/contacts">
-                  Contacts
-                </Link>
-              </li>
+                <li>
+                  <Link className="app-nav-link" to="/contacts">
+                    Contacts
+                  </Link>
+                </li>
 
-              <li>
-                <Button component={Link} to="/sign-in" radius="lg">
-                  Sign-in
-                </Button>
-              </li>
-            </ul>
-          </nav>
+                <li>
+                  <Button component={Link} to="/sign-in" radius="lg">
+                    Sign-in
+                  </Button>
+                </li>
+              </ul>
+            </nav>
+          ) : null}
         </div>
       </Container>
     </HeaderWrapper>
