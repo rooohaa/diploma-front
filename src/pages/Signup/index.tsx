992ebe5a73386b887be4591780b617241996f035
Tamerlan Bulatkaliyev <tameoooo13@gmail.com>
2023-03-12 14:28:38 +0600

    feat: added form stepper

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,6 +1,5 @@
-import { Container } from '@mantine/core';
+import { Box, Container, Stepper } from '@mantine/core';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { AppLayout } from '~/components';
 import {
   IAcademicPerformanceFormValues,
@@ -19,13 +18,10 @@ const forms = ['main', 'personal-info', 'academic-performance', 'last'];
 const SignUp: React.FC = () => {
   const [currentStep, setCurrentStep] = useState(0);
   const [formData, setFormData] = useState<IFormData | {}>({});
-  const navigate = useNavigate();
 
   const handleMoveNext = () => {
     if (currentStep < forms.length - 1) {
       setCurrentStep(currentStep + 1);
-    } else {
-      navigate('/');
     }
   };
 
@@ -39,24 +35,30 @@ const SignUp: React.FC = () => {
     handleMoveNext();
   };
 
-  const renderForms = () => {
-    switch (forms[currentStep]) {
-      case 'main':
-        return <Main onSubmit={handleSubmit} />;
-      case 'personal-info':
-        return <PersonalInfo onSubmit={handleSubmit} />;
-      case 'academic-performance':
-        return <AcademicPermormance onSubmit={handleSubmit} />;
-      case 'last':
-        return <Last />;
-      default:
-        return null;
-    }
-  };
-
   return (
     <AppLayout isAuthHeader={true}>
-      <Container size="lg">{renderForms()}</Container>
+      <Container size="lg">
+        <Box sx={{ marginTop: '20px' }}>
+          <Stepper
+            active={currentStep}
+            onStepClick={setCurrentStep}
+            allowNextStepsSelect={false}
+          >
+            <Stepper.Step label="Create an account">
+              <Main onSubmit={handleSubmit} />
+            </Stepper.Step>
+            <Stepper.Step label="Personal information">
+              <PersonalInfo onSubmit={handleSubmit} />
+            </Stepper.Step>
+            <Stepper.Step label="Academic permormance">
+              <AcademicPermormance onSubmit={handleSubmit} />
+            </Stepper.Step>
+            <Stepper.Completed>
+              <Last />;
+            </Stepper.Completed>
+          </Stepper>
+        </Box>
+      </Container>
     </AppLayout>
   );
 };
