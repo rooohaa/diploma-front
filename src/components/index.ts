601d45195014e0adc8afe915182cc14bad6e9368
Rakhat Alpysbai <rakhatdev@gmail.com>
2023-03-15 23:47:14 +0600

    add contact us form modal

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
-export { AppHeader } from './layout/AppHeader';
-export { AppLayout } from './layout/AppLayout';
-export { AppLogoLink } from './ui/AppLogoLink';
+export { AppHeader } from "./layout/AppHeader"
+export { AppLayout } from "./layout/AppLayout"
+export { AppLogoLink } from "./ui/AppLogoLink"
+export { ContactUsModal } from "./domain/ContactUsModal"
