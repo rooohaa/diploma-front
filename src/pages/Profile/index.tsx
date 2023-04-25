8eced8691438882e2aa99186c8cf00c1346e12ba
Rakhat Alpysbai <rakhatdev@gmail.com>
2023-04-26 00:59:24 +0600

    feat: resume pdf download

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,49 @@
+import { Button } from "@mantine/core"
+import axios from "axios"
 import { useMe } from "hooks/useMe"
+import { useState } from "react"
 
 const Profile: React.FC = () => {
+  const [loading, setLoading] = useState(false)
   const user = useMe()
 
-  return <div>Authenticated as : {user?.email}</div>
+  const handleDownloadResume = async () => {
+    setLoading(true)
+    try {
+      const res = await axios.get(
+        `${import.meta.env.VITE_APP_API_URL}/api/resume/pdf/`,
+        {
+          params: {
+            userId: user?.id,
+          },
+          responseType: "arraybuffer",
+        }
+      )
+
+      if (res.status === 200) {
+        const blob = new Blob([res.data], { type: "application/pdf" })
+
+        const link = document.createElement("a")
+        link.href = window.URL.createObjectURL(blob)
+        link.download = "resume.pdf"
+        link.click()
+      }
+    } catch (error) {
+      console.error(error)
+      alert("Service error, try later...")
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <>
+      <div>Authenticated as : {user?.email}</div>
+      <Button loading={loading} onClick={handleDownloadResume}>
+        Download resume
+      </Button>
+    </>
+  )
 }
 
 export { Profile }
